fe40df85b78be867b492908f5b6673123f6a373b
alphazhangyz <alphazhangyz@gmail.com>
2021-08-03 21:50:01 +0800

    一个超级简单基础的vuex

---
+++ vuex/index.js
@@ -0,0 +1,52 @@
+let _Vue = null;
+const EMPTY_OBJECT = {};
+class Store {
+  constructor(options) {
+    const {
+      state = EMPTY_OBJECT,
+      getters = EMPTY_OBJECT,
+      mutations = EMPTY_OBJECT,
+      actions = EMPTY_OBJECT
+    } = options;
+
+    this.state = _Vue.observable(state);
+    this.getters = Object.create(null);
+    Object.keys(getters).forEach(key => {
+      Object.defineProperty(this.getters, key, {
+        // configurable: true,
+        // enumerable: true,
+        get: () => getters[key](state),
+      })
+    })
+
+    this._mutations = mutations;
+    this._actions = actions;
+  }
+
+  commit (type, payload) {
+    this._mutations[type](this.state, payload);
+  }
+
+  dispatch (type, payload) {
+    this._actions[type](this, payload);
+  }
+}
+
+function install(Vue) {
+  _Vue = Vue;
+
+  _Vue.mixin({
+    beforeCreate() {
+      if (this.$options.store) {
+        _Vue.prototype.$sore = this.$options.store;
+      }
+    },
+  })
+
+
+}
+
+export default {
+  Store,
+  install
+}
